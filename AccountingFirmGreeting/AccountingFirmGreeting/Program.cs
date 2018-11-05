c33081653f7f65c5021e1443c3200b305499fb59
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-05 16:35:33 -0500

    Add files via upload

---
+++ AccountingFirmGreeting/AccountingFirmGreeting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace AccountingFirmGreeting
+{
+    class Program
+    {
+        static void Main()
+        {
+            String str = @"""accounting""";
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            
+            Console.WriteLine("Remember, we're " + str + " on you !");
+            Console.Read();
+        }
+    }
+}
