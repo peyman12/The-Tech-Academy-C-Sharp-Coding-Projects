0a9a87eadde3e86fd51d932328ca290140870056
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-19 09:59:30 -0500

    Add files via upload

---
+++ Branching/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            double packWeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package width:");
+            double packWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            double packHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            double packLength = Convert.ToInt32(Console.ReadLine());
+
+            double sumDimensions = (packLength + packWidth + packHeight);
+
+            if (sumDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+                double totalCost = ((sumDimensions * packWeight) / 100);
+                Console.WriteLine("Your estimated total for shipping this package is: $" + totalCost);
+                Console.WriteLine("Thank you.");
+                Console.ReadLine();
+            }
+
+
+
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+
+
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Something went wrong.");
+            //}
+
+
+
+            //int currentTemperature = 70;
+            //int roomTemperature = 60;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+
+            Console.ReadLine();
+        
+        }
+
+    }
+}
