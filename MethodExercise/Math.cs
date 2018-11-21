105e11aaf0a4aee6b284a481addf8d9592514262
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-21 18:19:37 -0500

    Add files via upload

---
+++ MethodExercise/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    class Math
+    {
+        public int Addition(int numInput)
+        {
+            int numOutput = (numInput + 1);
+            return numOutput;
+        }
+
+        public int Subtraction(int numInput)
+        {
+            int numOutput = (numInput - 1);
+            return numOutput;
+        }
+        public int Multiply(int numInput)
+        {
+            int numOutput = (numInput * 1);
+            return numOutput;
+        }
+    }
+
+}
---
+++ MethodExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mymathbook1 = new Math();
+
+
+            Console.WriteLine("What number do you want to add 1 to?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your result is " + mymathbook1.Addition(userInput));
+            
+
+            Console.WriteLine("What number do you want subtract 1 from?");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your result is " + mymathbook1.Subtraction(userInput));
+            
+
+            Console.WriteLine("What number do you want to muiltple 1 to?");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your result is " + mymathbook1.Multiply(userInput));
+            Console.ReadLine();
+        }
+
+    }
+}
