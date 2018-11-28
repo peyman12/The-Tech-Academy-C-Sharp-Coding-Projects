571b001433b1265172fb6f6ea34c16622b6f5d2f
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-28 14:18:37 -0500

    Add files via upload

---
+++ ClassMethods/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+    //static class cannot be instantiated in program.cs
+{
+    public static class Math
+    {   //divide method is considered overloaded b/c there are 2 set of instructions for the one method
+        //void means cannot return anything
+        public static void Divide(int numInput)
+        {
+            int numOutput = (numInput / 2);
+            Console.WriteLine(numOutput);
+            Console.ReadLine();
+        }
+        public static void Divide(string numInput)
+        {
+            int numOutput = (Convert.ToInt32(numInput) / 2);
+            Console.WriteLine(numOutput);
+            Console.ReadLine();
+        }
+        public static void Multiply(int numInput, out int numOutput2)
+        {
+             numOutput2 = 0;
+             
+        }
+    }
+}
---
+++ ClassMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            
+            int numInput = Convert.ToInt32(Console.ReadLine());
+            Math.Divide(numInput);
+
+            
+            
+        }
+    }
+}
