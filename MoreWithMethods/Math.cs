1c0e6c96d8ece4488628281c315935be48593332
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-27 19:57:09 -0500

    Add files via upload

---
+++ MoreWithMethods/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods
+{
+    class Math
+    {
+        public void Add(int num1, int num2)
+        {
+            int result = num1 + 5;
+
+            Console.WriteLine(num2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MoreWithMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Math mymathbook1 = new Math();
+
+            //these are 2 diffrent examples that run through the math method creating 4 results
+            mymathbook1.Add(2, 3);
+            mymathbook1.Add(num1: 3, num2: 7);
+            Console.ReadLine();
+        }
+    }
+}
