ac8823cf2c754ee34abcccfe631d9fadfc0a66bc
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-28 14:14:36 -0500

    Add files via upload

---
+++ MoreWithMethods3/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods3
+{
+    class Math
+    {
+       
+        public int Add(int numInput1, int x = 0)
+        {
+            int numOutput2 = (numInput1 + x);
+            return numOutput2;
+
+        }
+    }
+
+}
---
+++ MoreWithMethods3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mybook1 = new Math();
+            Console.WriteLine("Input a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number if you'd like.");
+            string userInput2 = Console.ReadLine();
+
+            
+
+            if (userInput2 == "")
+            {
+                int userOutput = mybook1.Add(userInput);
+
+                Console.WriteLine(userOutput);
+            }
+           else
+            {
+                int userOutput = mybook1.Add(userInput, Convert.ToInt32(userInput2));
+
+                Console.WriteLine(userOutput);
+            }
+            
+            Console.ReadLine();
+            
+            
+
+            
+        }
+    }
+}
