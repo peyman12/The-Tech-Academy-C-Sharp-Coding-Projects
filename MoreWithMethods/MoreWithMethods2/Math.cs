faf72055a02c2c6c3ba128abf37ce7212362cf64
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-28 08:31:17 -0500

    Add files via upload

---
+++ MoreWithMethods/MoreWithMethods2/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods2
+{
+    class Math
+    {
+        public int Add(int numInput)
+        {
+            int numOutput = (numInput + 4);
+            return numOutput;
+        }
+        // public int b/c answer is asked to be in integer
+        public int Add1(double numInput2)
+        {
+            int numOutput2 = (Convert.ToInt32(numInput2) + 7);
+            return numOutput2;
+        }
+        
+        public int Add2(string stringInput)
+        {
+            int stringOutput = (Convert.ToInt32(stringInput) + 8);
+            return stringOutput;
+        }
+    }
+}
---
+++ MoreWithMethods/MoreWithMethods2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreWithMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mymathbook1 = new Math();
+            Console.WriteLine("Input your first number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your result is: " + mymathbook1.Add(userInput));
+
+            Console.WriteLine("Input your second number");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your result is: " + mymathbook1.Add1(userInput2));
+
+            Console.WriteLine("Input your second number");
+            string userInput3 = Console.ReadLine();
+            Console.WriteLine("Your result is: " + mymathbook1.Add2(userInput3));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
