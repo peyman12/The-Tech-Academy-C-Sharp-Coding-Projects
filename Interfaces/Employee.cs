b24f4a84934aa8d4e520d1942e0a77ea1e57ba54
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-29 18:36:49 -0500

    Add files via upload

---
+++ Interfaces/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    
+    
+        public class Employee : Person, IQuittable
+        {
+            public override void SayName()
+            {
+                Console.WriteLine(FirstName + " " + LastName);
+            }
+            public void Quit()
+            {
+                Console.WriteLine("I, " + FirstName + " " +
+                    LastName + ", do hereby Quit!");
+            }
+        }
+    }
+
---
+++ Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable name = new Employee();
+            Employee employees = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            //hitting enter quits the program
+            employees.SayName();
+            Console.ReadLine();
+            name.Quit();
+        }
+    }
+}
