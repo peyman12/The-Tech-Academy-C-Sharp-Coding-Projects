ceb1f336d3c0802829146a6964f87665dc738520
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-28 21:01:08 -0500

    Add files via upload

---
+++ Inheritance/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        //these are properties aka object variables with get post functionality
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public  void SayName()
+        {
+            
+            Console.WriteLine("Name: " + FirstName + LastName);
+            Console.ReadLine();
+        }
+        
+        
+
+        
+    }
+}
