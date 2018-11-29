48b64c5455a1855f2484a6fb2a4d806099addffb
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-29 16:59:01 -0500

    Add files via upload

---
+++ AbstractMethods/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AbstractMethods;
+using AbstractClasses;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMethods
+{
+    public  class Employee : Person
+    {  
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+        
+    }
+}
