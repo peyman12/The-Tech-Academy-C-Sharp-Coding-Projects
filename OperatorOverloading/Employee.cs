e90839df0979c9b68add1d48673618e2c284bcdc
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-30 13:11:05 -0500

    Add files via upload

---
+++ OperatorOverloading/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Employee : Person, IQuittable
+    {
+        
+            int employeeId;
+
+            public Employee()
+            {
+                employeeId = 0;
+            }
+            public Employee(int x)
+            {
+                employeeId = x;
+            }
+
+        public static bool operator ==(Employee x, Employee y)
+        {
+            if (x.employeeId == y.employeeId)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Employee x, Employee y)
+        {
+            if (x.employeeId != y.employeeId)
+                return true;
+            else
+                return false;
+        }
+
+        public int getValue()
+        {
+            return employeeId;
+        }
+
+
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
+    
+
---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {  
+            static void Main(string[] args)
+            {
+            //by changing the id values in brackets we can execute either the if statement of the else statement
+            Employee id1 = new Employee(1);
+            Employee id2 = new Employee(2);
+
+
+            if (id1 == id2)
+            {
+                Console.WriteLine("The Employee Ids are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The Employee Ids are different.");
+            }
+            Console.ReadLine();
+            }
+        static void Quit(IQuittable employee)
+        {
+            employee.Quit();
+        }
+    }
+ }
+    
+
