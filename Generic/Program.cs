c10c7da4cd0ac52675691d02e4e4e645e5cfa8c7
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-11-30 14:06:11 -0500

    Add files via upload

---
+++ Generic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+
+            employee.Things = new List<string> { "Doug", "Jim", "Rory", "Walt" };
+
+            Employee<int> employee2 = new Employee<int>();
+
+            employee2.Things = new List<int> { 2, 4, 5, 9 };
+
+            foreach (string item in employee.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in employee2.Things)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
