48921ae8e1c7473d549f707aed634f085a7b4e95
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-12-10 06:59:50 -0500

    Add files via upload

---
+++ DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine(currentTime);
+
+                Console.WriteLine("Input a number. The time will be modified in hours, based on the input.");
+                var hours = Convert.ToInt32(Console.ReadLine());
+
+                DateTime timeInHours = currentTime.AddHours(hours);
+                Console.WriteLine(timeInHours);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
