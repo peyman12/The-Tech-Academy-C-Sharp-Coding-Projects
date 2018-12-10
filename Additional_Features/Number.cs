e3c90be5d8f50473450b9f99eedf8953196b305f
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-12-10 17:40:00 -0500

    Add files via upload

---
+++ Additional_Features/Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Number
+    {
+        public string Name { get; set; }
+        public int Num { get; set; }
+
+        public Number(string name, int num)
+        {
+            Name = name;
+            Num = num;
+        }
+        public Number() : this("Subtracting 1 is from 2 is", 1)
+        {
+        }
+    }
+}
+
---
+++ Additional_Features/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                const int currentNumber = 2;
+                Console.WriteLine("The current value is {0}", currentNumber);
+
+                var previousNumber = new Number("That current value subtract 1 is ", 1);
+                Console.WriteLine("The number before that is {0}", previousNumber.Num);
+                Console.ReadLine();
+            }
+        
+    }
+}
