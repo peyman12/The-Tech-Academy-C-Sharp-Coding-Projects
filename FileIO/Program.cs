f41c51845f423e7553e19678742a1375fee0efac
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-12-10 06:40:22 -0500

    Add files via upload

---
+++ FileIO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Input a statement. It will be logged and a file will be created on this computer.");
+                string text = Console.ReadLine();
+                File.WriteAllText("C:\\Users\\User\\Documents\\Visual Studio 2017\\fileio.txt", text);
+                string fileRead = File.ReadAllText("C:\\Users\\User\\Documents\\Visual Studio 2017\\fileio.txt");
+                Console.WriteLine("Statement logged: " + " " + fileRead);
+                Console.ReadLine();
+            }
+        }
+    }
+}
