fa5dff32a8ca889136e9e42257e1e8db66d35f30
peyman12 <36832426+peyman12@users.noreply.github.com>
2018-12-11 11:10:30 -0500

    Add files via upload

---
+++ MoreExceptionHandling/Age.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptionHandling
+{
+     public class Age : Exception
+        {
+            public Age()
+               : base() { }
+            public Age(String message)
+                : base(message) { }
+        }
+}
+
---
+++ MoreExceptionHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your age.");
+            try
+            {
+                bool validAnswer = false;
+                int age = 1;
+                while (!validAnswer)
+                {
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Enter a whole number greater than zero.");
+                }
+                if (age <= 0)
+                {
+                    throw new Age();
+                }
+
+                DateTime currentYear = DateTime.Now;
+                int CurrentYear = Convert.ToInt32(currentYear.Year);
+                int YearOfBorn = CurrentYear - age;
+                Console.WriteLine("Your birth year is {0}", YearOfBorn);
+                Console.ReadLine();
+            }
+            catch (Age)
+            {
+                Console.WriteLine("An error occurred. Make sure to input positive and has no decimals.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please contact support.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
